92f480475e084dec9bab03664fd5c0d359351348
zong_ahri <choijoy10@naver.com>
2024-05-29 08:21:35 +0900

    프로젝트에 Spring Data Jpa 적용

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Getter // 클래스 내 모든 필드의 Getter 메소드를 자동생성
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity // 테이블과 링크될 클래스
+public class Posts {
+
+    @Id // 해당 테이블의 PK 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성 규칙, 스프링 부트 2.0 에서는 GenerationType.IDENTITY 옵션을 추가해야만 auto_increment가 됨
+    private Long id;
+
+    @Column(length = 500, nullable = false) // 테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 칼럼이 됨
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
