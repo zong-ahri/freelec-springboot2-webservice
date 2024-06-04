b6bc3c6c0e741984b75677ac0b167a9353a9b7aa
zong_ahri <choijoy10@naver.com>
2024-06-04 17:40:44 +0900

    등록/수정/조회 API 만들기

--- src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter // 선언된 모든 필드의 get 메소드를 생성
-@RequiredArgsConstructor // 선언된 모단 final 필드가 포함된 생성자를 생성, final이 없는 필드는 생성자에 포함x
+@RequiredArgsConstructor // 선언된 모든 final 필드가 포함된 생성자를 생성, final이 없는 필드는 생성자에 포함x
 public class HelloResponseDto {
 
     private final String name;
