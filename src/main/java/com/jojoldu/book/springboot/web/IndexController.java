bdaa623ed75d6a6189c05e5e1fb7980c66adcaf6
zong_ahri <choijoy10@naver.com>
2024-06-21 15:56:20 +0900

    어노테이션 코드 개선

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
@@ -18,10 +19,10 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) { // @LoginUser SessionUser user : 기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
 
         if (user != null) { // 세션에 저장된 값이 있을 때만 model에 userName으로 등록
             model.addAttribute("userName", user.getName());
